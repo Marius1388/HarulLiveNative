75763b8087334341f1b8ea61bc7f3e61935ac233
Raul Filea <raulfilea@gmail.com>
2023-04-25 18:50:44 +0300

    Deleted line that displayed video controls and removed unused lines.

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
-import { useRef, useState } from 'react';
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Video } from 'expo-av';
+import { useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Video } from "expo-av";
 
 export default function App() {
-	const video = useRef(null);
-	const [status, setStatus] = useState({});
-	return (
-		<View style={styles.container}>
-			{
-				<Video
-					ref={video}
-					style={styles.video}
-					source={{
-						uri: 'https://58f9d53e45ea8.streamlock.net:443/wildliferomania2/live.stream_aac/manifest.mpd',
-					}}
-					useNativeControls
-					resizeMode="contain"
-					// isLooping
-					onPlaybackStatusUpdate={setStatus}
-				/>
-			}
-		</View>
-	);
+  const video = useRef(null);
+  const [setStatus] = useState({});
+  return (
+    <View style={styles.container}>
+      {
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: "https://58f9d53e45ea8.streamlock.net:443/wildliferomania2/live.stream_aac/manifest.mpd",
+          }}
+          resizeMode="contain"
+          onPlaybackStatusUpdate={setStatus}
+        />
+      }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	video: {
-		flex: 1,
-		alignSelf: 'stretch',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#0f2d41",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  video: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
 });
